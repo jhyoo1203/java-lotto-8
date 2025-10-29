d6bce9b0d9c819c48ef81c6391bc0314a46c6dd9
JaeHyeon Yoo <jhyoo1203@gmail.com>
2025-10-29 19:50:34 +0900

    feat: 로또 랭킹 시스템을 나타내기 위한 enum 클래스 추가

---
+++ src/main/java/lotto/domain/LottoRank.java
@@ -0,0 +1,42 @@
+package lotto.domain;
+
+public enum LottoRank {
+
+    FIFTH(3, false, 5_000),
+    FOURTH(4, false, 50_000),
+    THIRD(5, false, 1_500_000),
+    SECOND(5, true, 30_000_000),
+    FIRST(6, false, 2_000_000_000)
+    ;
+
+    private final int matchCount;
+    private final boolean hasBonus;
+    private final int prize;
+
+    LottoRank(int matchCount, boolean hasBonus, int prize) {
+        this.matchCount = matchCount;
+        this.hasBonus = hasBonus;
+        this.prize = prize;
+    }
+
+    public static LottoRank from(int matchCount, boolean bonusMatch) {
+        for (LottoRank rank : values()) {
+            if (rank.matchCount == matchCount && rank.hasBonus == bonusMatch) {
+                return rank;
+            }
+        }
+        return null;
+    }
+
+    public int getMatchCount() {
+        return matchCount;
+    }
+
+    public boolean hasBonus() {
+        return hasBonus;
+    }
+
+    public int getPrize() {
+        return prize;
+    }
+}
